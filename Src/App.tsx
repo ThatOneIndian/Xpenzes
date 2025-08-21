318705c0516aabf935c9747a78dad186b3570695
Kirtoisplayz <omgitsdarshan@gmail.com>
2025-08-21 11:53:10 -0700

    Update App.tsx

--- Src/App.tsx
+++ Src/App.tsx
@@ -6,6 +6,7 @@ import { supabase } from './utils/supabase';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={{ fontSize: 80, fontWeight: 'bold', height : 500, alignItems : 'center' }}>Welcome {'\n'} To Expenzes!</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Text>Welcome to Supabase with React Native!</Text>
       <StatusBar style="auto" />
@@ -16,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
