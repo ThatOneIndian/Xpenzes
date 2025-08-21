11d805843878c92e97302027162106608c4561a7
Kirtoisplayz <omgitsdarshan@gmail.com>
2025-08-21 15:54:13 -0700

    Add Expo Router package to app files

--- Src/App.tsx
+++ Src/App.tsx
@@ -1,23 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { Link, Router } from 'expo-router';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import { supabase } from './utils/supabase';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 80, fontWeight: 'bold', height : 500, alignItems : 'center' }}>Welcome {'\n'} To Expenzes!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Welcome to Supabase with React Native!</Text>
+      <Text style={{ fontSize: 80, fontWeight: 'bold', height : 600, alignItems : 'center', justifyContent: 'center' }}>Welcome To Expenzes!</Text>
+      <Text style={{ fontSize: 25, height: 100 }}>Swipe to the left to continue!</Text>
       <StatusBar style="auto" />
+      <Link href="/about">About</Link>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    fontSize: 20,
     flex: 1,
-    backgroundColor: '#ffffffff',
+    backgroundColor: '#63d963',
     alignItems: 'center',
     justifyContent: 'center',
   },
