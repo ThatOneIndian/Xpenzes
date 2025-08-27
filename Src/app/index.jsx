142242d0072d267729b836936de3515d8b9d4f3d
Kirtoisplayz <omgitsdarshan@gmail.com>
2025-08-26 17:15:07 -0700

    Create home page with a temp link to landing page

--- Src/app/index.jsx
+++ Src/app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { getRouteConfigSorter } from "expo-router/build/fork/getStateFromPath-forks";
 import { StyleSheet, Text, View, Image } from "react-native";
 
@@ -5,6 +6,7 @@ const Home = () => {
     return (
         <View>
             <Text>Welcome to the Home Screen</Text>
+            <Link href='/landing'>Hello</Link>
         </View>
     )
 }
