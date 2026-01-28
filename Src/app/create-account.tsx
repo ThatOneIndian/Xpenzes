3070bc2cf0d88ad21c2d519d450f28108c094c94
Kirtoisplayz <omgitsdarshan@gmail.com>
2026-01-27 19:54:45 -0800

    create an create account page

---
+++ Src/app/create-account.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native"
+
+export default function CreateAccountScreen() {
+  return (
+    <View>
+      <Text>
+        Sign In Screen
+      </Text>
+    </View>
+  );
+}
