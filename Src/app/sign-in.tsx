591360d4df4ddcebf4400d02d47b50bb2b7b21a6
Kirtoisplayz <omgitsdarshan@gmail.com>
2026-02-06 17:40:54 -0800

    add sign in button that leads to create account based on auth state

--- Src/app/sign-in.tsx
+++ Src/app/sign-in.tsx
@@ -1,15 +1,18 @@
 import { Link } from "expo-router";
 import { Button } from "react-native";
 import { View, Text, StyleSheet } from "react-native"
+import { useAuthStore } from '../utils/authStore';
 
 export default function SignInScreen() {
+  const { logIn } = useAuthStore();
   return (
     <View>
       <Text>
         Sign In Screen
       </Text>
+      <Button title="Sign in" onPress={logIn} />
       <Link asChild push href="/modal">
-        <Button title="Open modal" />
+        <Button title="Open modal (disabled)" />
       </Link>
     </View>
   );
