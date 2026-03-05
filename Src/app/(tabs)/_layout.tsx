0192a22fbb5dc0d5169fda3d41cbd21b10efda56
Kirtoisplayz <omgitsdarshan@gmail.com>
2026-03-04 20:59:34 -0800

    create group tab, update layout

--- Src/app/(tabs)/_layout.tsx
+++ Src/app/(tabs)/_layout.tsx
@@ -4,6 +4,7 @@ export default function TabsLayout() {
     return (
     <Tabs>
         <Tabs.Screen name="index" />
+        <Tabs.Screen name="groups" />
         <Tabs.Screen name="settings" />
     </Tabs>
     );
---
+++ Src/app/(tabs)/groups.tsx
@@ -0,0 +1,9 @@
+import { View, Text} from "react-native"
+
+export default function groups() {
+  return (
+    <View>
+      <Text>Groups screen</Text>
+    </View>
+  );
+}
