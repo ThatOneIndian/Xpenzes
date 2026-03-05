82b8588c90b7660b84c9ab43494a343c0d309366
Kirtoisplayz <omgitsdarshan@gmail.com>
2026-03-04 21:00:29 -0800

    rm redundant button

--- Src/app/(tabs)/index.jsx
+++ Src/app/(tabs)/index.jsx
@@ -1,13 +1,9 @@
 import { Text, View, Button } from 'react-native';
-import { Link } from "expo-router"
 
 export default function Index() {
   return (
     <View>
       <Text>Home screen</Text>
-      <Link asChild push href="/modal">
-        <Button title="Open modal" />
-      </Link>
     </View>
   );
 }
