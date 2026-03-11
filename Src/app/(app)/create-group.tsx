36e78d2b3fd3f0aca60df7437c3fecbfce537c61
Kirtoisplayz <omgitsdarshan@gmail.com>
2026-03-11 14:22:58 -0700

    boilerplate text and styling for boilerplate

--- Src/app/(app)/create-group.tsx
+++ Src/app/(app)/create-group.tsx
@@ -3,14 +3,24 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function createGroup() {
 
     return(
-        <View>
-            <Text>Great Groot</Text>
+        <View style={styles.container}>
+            <Text style={styles.header}>Create a Group!</Text>
+            <Text>Name</Text>
+            <Text>Description(Optional)</Text>
+            <Text>Add members</Text>
         </View>
     );
 
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        backgroundColor: "green"
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: "bold",
+        textAlign: "center"
+    }
 
 });
