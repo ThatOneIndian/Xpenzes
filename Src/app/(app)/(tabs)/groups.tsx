83ac85de3c3d185315c8b64fe5244a783a5967f8
Kirtoisplayz <omgitsdarshan@gmail.com>
2026-03-10 19:12:39 -0700

    create dummy data

--- Src/app/(app)/(tabs)/groups.tsx
+++ Src/app/(app)/(tabs)/groups.tsx
@@ -3,9 +3,13 @@ import { View, Text, Button,StyleSheet} from "react-native"
 
 export default function groups() {
   const router = useRouter();
-
   const goToCreateGroup = () => { router.push('/create-group') };
 
+  const dummyGroups = [
+    {id: '1', name:'group 1', amountOwed: 0, amountToPay: 100},
+    {id: '2', name:'Japan Trip 2026', amountOwed: 100, amountToPay: 10},
+    {id: '3', name:'Roommates', amountOwed: 120, amountToPay: 0}
+  ];
   return (
     <View>
       <Text>Groups screen</Text>
